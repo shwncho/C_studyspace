4ecd2aad7b8b474fdc6a355eda87ffcb5298a237
simple <shwncho4@gmail.com>
2022-04-07 00:16:08 +0900

    Thread

---
+++ OS/createThread.c
@@ -0,0 +1,32 @@
+#include<pthread.h>
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<unistd.h>
+
+int value = 0;
+void *runner(void *param);
+
+int main(int argc, char *argv[])
+{
+pid_t pid;
+pthread_t tid;
+pthread_attr_t attr;
+
+	pid=fork();
+
+	if(pid==0){
+		pthread_attr_init(&attr);
+		pthread_create(&tid,&attr,runner,NULL);
+		pthread_join(tid,NULL);
+		printf("CHILD: value = %d",value);
+	}
+	else if(pid>0){
+		printf("PARENT: value = %d", value);
+	}
+}
+
+void *runner(void *param){
+	value=5;
+	pthread_exit(0);
+}
