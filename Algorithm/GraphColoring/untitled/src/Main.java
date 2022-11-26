1080818143f86923fd158866ec36fb3e8d19cbd4
simple <shwncho4@gmail.com>
2022-11-26 16:11:33 +0900

    refactor: graph coloring

--- Algorithm/GraphColoring/untitled/src/Main.java
+++ Algorithm/GraphColoring/untitled/src/Main.java
@@ -4,47 +4,60 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[] vcolor;
-    static boolean[][] W;
+    static int[] vcolor; // 각 노드별 색깔
+    static boolean[][] W; // State Space Tree
+    //n x n 배열
     static int n;
+    // 사용한 색깔 갯수
+    static int m;
+    //색칠가능한 경우의 수
     static int cnt=0;
-    static boolean promising(int x){
+    static boolean promising(int i){
+        //State Space Tree 탐색
         int j=1;
-
-        while(j<x){
-            if(W[x][j] && vcolor[j-1] == vcolor[j])   return false;
+        boolean flag=true;
+        while(j<i && flag){
+            //(i,j)가 연결되어 있으며, 두 노드의 색깔이 같은 경우
+            if(W[i][j] && vcolor[i] == vcolor[j]){
+                flag=false;
+            }
             j++;
         }
-        return true;
+        return flag;
     }
-    static void DFS(int x){
-        if(x==n)    cnt++;
-        else{
-            for(int color=1; color<=2; color++){
-                vcolor[x]=color;
-                if(promising(x))    DFS(x+1);
+    static void DFS(int i){
+        if(promising(i)){
+            if(i==n){
+                cnt++;
             }
+            else{
+                for(int color=1; color<=m; color++){
+                    vcolor[i+1]=color;
+                    DFS(i+1);
+                }
+            }
+
         }
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        // 노드 n개 m개의 간선을 입력받는다고 가정 -> n x m 그래프
-        st=new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        // n x n 그래프
+        n = Integer.parseInt(br.readLine());
 
         vcolor=new int[n+1];
         W=new boolean[n+1][n+1];
-        cnt=0;
-        for(int j=0; j<m; j++){
+        for(int i=1; i<=n; i++){
             st=new StringTokenizer(br.readLine());
-            int a =Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            W[a][b]=true;
-            W[b][a]=true;
+            for(int j=1; j<=n; j++){
+                if(Integer.parseInt(st.nextToken())==1){
+                    W[i][j]=true;
+                    W[j][i]=true;
+                }
+            }
         }
+        m=3;
         DFS(1);
         System.out.println(cnt);
 
